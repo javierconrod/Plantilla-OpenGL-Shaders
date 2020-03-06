4ebffdba80ca376d4f5cce6c16a5673d1a10bb28
alumno <alumno@E301>
2020-03-06 10:48:33 -0700

    Mapeo de atributos y asociar

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -21,7 +21,7 @@ using namespace std;
 
 //Cada elemento que queramos renderear necesita un vertex array y un buffer
 vector<Vertice>triangulo;
-GLuint vertexArrayTriangulo;
+GLuint vertexArrayTrianguloID;
 GLuint bufferTrianguloID;
 
 //Instancia del shader
@@ -94,6 +94,32 @@ int main()
 	const char* rutaFragmentShader = "FragmentShader.shader";
 
 	shader = new Shader(rutaVertexShader, rutaFragmentShader);
+
+	//Mapeo de atributos
+	posicionID = glGetAttribLocation(shader->getID(), "posicion");
+	colorID = glGetAttribLocation(shader->getID(), "color");
+
+	shader->desenlazar();
+
+	//Crear el vertex array del triangulo
+	glGenVertexArrays(1, &vertexArrayTrianguloID);
+	glBindVertexArray(vertexArrayTrianguloID);
+	
+	//Vertex buffer
+	glGenBuffers(1, &bufferTrianguloID);
+	glBindBuffer(GL_ARRAY_BUFFER, bufferTrianguloID);
+
+	//Asociar datos al buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertice) * triangulo.size(), triangulo.data(), GL_STATIC_DRAW);
+	
+	//Habilitar atributos de shader
+	glEnableVertexAttribArray(posicionID);
+	glEnableVertexAttribArray(colorID);
+	
+	//Especificar a OpenGL cómo comunicarse
+	glVertexAttribPointer(posicionID, 3, GL_FLOAT, GL_FALSE, sizeof(Vertice), 0);
+
+
 
 	//Ciclo de dibujo (DrawLoop)
 	while (!glfwWindowShouldClose(window)) {
